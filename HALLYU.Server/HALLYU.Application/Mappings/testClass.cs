dd8b3fc93b2d6b73b50436aba21de7fed54ed3c8
DEV\Danila.Denisenko <tinder12347@mail.ru>
2024-10-02 23:41:17 +0300

    initial server infrastructure

---
+++ HALLYU.Server/HALLYU.Application/Mappings/testClass.cs
@@ -0,0 +1,10 @@
+﻿namespace HALLYU.Application.Mappings
+{
+    /// <summary>
+    /// test
+    /// </summary>
+    public class TestClass
+    {
+        public int ARR { get; set; } = 1;
+    }
+}
---
+++ HALLYU.Server/HALLYU.Domain/Contracts/IAuditableEntity.cs
@@ -0,0 +1,16 @@
+﻿using EDO_FOMS.Domain.Contracts;
+
+public interface IAuditableEntity<TId> : IAuditableEntity, IEntity<TId>
+{
+}
+
+public interface IAuditableEntity : IEntity
+{
+    string CreatedBy { get; set; }
+
+    DateTime CreatedOn { get; set; }
+
+    string LastModifiedBy { get; set; }
+
+    DateTime? LastModifiedOn { get; set; }
+}
